b3bd12bbcf9cf33670c111664b43ace2884c2e22
kbmke <kayla.ball222@gmail.com>
2023-08-14 17:42:23 -0500

    added an about me area

--- .php_tmp/nav.php
+++
@@ -1,7 +0,0 @@
-<header id="nav">
-    <nav>
-      <a href="/index.html" class="nav-home nav-button">Home</a>
-      <a href="fem/fem.html" class="nav-fem nav-button">FEM Challenges</a>
-      <a href="">Personal Projects</a>
-    </nav>
-  </header>
